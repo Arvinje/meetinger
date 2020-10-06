29c0a324c144604189ae4572f6f759a93656ae8b
Arvin Jenabi <arvinje@gmail.com>
2020-10-07 01:10:36 +0300

    Add BaseController

---
+++ backend/src/shared/infra/http/BaseController.ts
@@ -0,0 +1,43 @@
+/* eslint-disable class-methods-use-this */
+import { APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayWithAuthorizerEvent } from './types';
+
+export abstract class BaseController {
+  protected abstract execute(event: APIGatewayWithAuthorizerEvent): Promise<APIGatewayProxyResult>;
+
+  protected static json<TResponse>(statusCode: number, body?: TResponse): APIGatewayProxyResult {
+    const jsonBody = body ? JSON.stringify(body) : '';
+    return {
+      statusCode,
+      body: jsonBody,
+    };
+  }
+
+  protected ok<TResponse>(body?: TResponse): APIGatewayProxyResult {
+    return BaseController.json(200, body);
+  }
+
+  protected created<TResponse>(body?: TResponse): APIGatewayProxyResult {
+    return BaseController.json(201, body);
+  }
+
+  protected unauthorized<TResponse>(body?: TResponse): APIGatewayProxyResult {
+    return BaseController.json(401, body);
+  }
+
+  protected forbidden<TResponse>(body?: TResponse): APIGatewayProxyResult {
+    return BaseController.json(403, body);
+  }
+
+  protected notFound<TResponse>(body?: TResponse): APIGatewayProxyResult {
+    return BaseController.json(404, body);
+  }
+
+  protected unprocessableEntity<TResponse>(body?: TResponse): APIGatewayProxyResult {
+    return BaseController.json(422, body);
+  }
+
+  protected internalError<TResponse>(body?: TResponse): APIGatewayProxyResult {
+    return BaseController.json(500, body);
+  }
+}
---
+++ backend/src/shared/infra/http/types.ts
@@ -0,0 +1,16 @@
+import {
+  APIGatewayProxyWithLambdaAuthorizerEvent,
+  APIGatewayProxyWithLambdaAuthorizerHandler,
+} from 'aws-lambda';
+
+export interface AuthorizerContext {
+  username: string;
+}
+
+export type APIGatewayWithAuthorizerEvent = APIGatewayProxyWithLambdaAuthorizerEvent<
+  AuthorizerContext
+>;
+
+export type APIGatewayWithAuthorizerHandler = APIGatewayProxyWithLambdaAuthorizerHandler<
+  AuthorizerContext
+>;
