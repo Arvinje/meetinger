f54519f93c606224b1e4611aa6c3950e55157d34
Arvin Jenabi <arvinje@gmail.com>
2020-10-16 15:18:51 +0300

    Remove old handler

--- backend/src/handler.ts
+++
@@ -1,7 +0,0 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
-
-export const hello: APIGatewayProxyHandler = async (event) => ({
-  statusCode: 200,
-  body: JSON.stringify(event.body, null, 2),
-});
