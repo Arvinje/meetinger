01f9e2455f7304463213776e32362cdc91808eaf
Arvin Jenabi <arvinje@gmail.com>
2020-10-17 15:48:59 +0300

    Remove frontend for now

--- frontend/src/aws-config.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export default {
-    aws_project_region: 'eu-central-1',
-    Auth: {
-        region: 'eu-central-1',
-        userPoolId: 'eu-central-1_JA8adtK4M',
-        userPoolWebClientId: '5thombhjif6hqhf580kas211pm',
-    }
-}
