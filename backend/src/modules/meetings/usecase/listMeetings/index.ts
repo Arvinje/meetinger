999189961877a128dd24b9aa1dcdb0e01637cbfb
Arvin Jenabi <arvinje@gmail.com>
2020-12-23 00:15:17 +0200

    Fix ListMeetingsController file name

--- backend/src/modules/meetings/usecase/listMeetings/index.ts
+++ backend/src/modules/meetings/usecase/listMeetings/index.ts
@@ -3,7 +3,7 @@ import DDBConfig from '@src/shared/infra/dynamodb/dynamodb';
 import { APIGatewayWithAuthorizerHandler } from '@src/shared/infra/http/types';
 import { DynamoDBMeetingRepo } from '@meetings/repos/implementations/DynamoDBMeetingRepo';
 import { ListMeetingsUseCase } from './ListMeetingsUseCase';
-import { ListMeetingsController } from './GetMeetingController';
+import { ListMeetingsController } from './ListMeetingsController';
 
 // Repositories
 const meetingRepo = new DynamoDBMeetingRepo(DDBConfig);
