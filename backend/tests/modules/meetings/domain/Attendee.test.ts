6a2988acb4f05dc96ecf76424d57db491fa1c172
Arvin Jenabi <arvinje@gmail.com>
2021-02-24 09:46:31 +0200

    Add tests for Attendee AggregateRoot

---
+++ backend/tests/modules/meetings/domain/Attendee.test.ts
@@ -0,0 +1,129 @@
+import faker from '@tests/support/faker';
+import { UserName } from '@src/modules/users/domain/UserName';
+import { UserFullName } from '@src/modules/users/domain/UserFullName';
+import { MeetingID } from '@src/modules/meetings/domain/MeetingID';
+import dayjs from 'dayjs';
+import { Attendee } from '@src/modules/meetings/domain/Attendee';
+import { MeetingTitle } from '@src/modules/meetings/domain/MeetingTitle';
+
+describe('Attendee AggregateRoot', () => {
+  describe('create a new Attendee', () => {
+    it('returns a new attendee given all args', async () => {
+      expect.hasAssertions();
+
+      const username = (await UserName.create(faker.random.alphaNumeric(6))).unwrap();
+      const fullName = (await UserFullName.create(faker.name.findName())).unwrap();
+      const meetingID = MeetingID.create();
+      const joinedMeetingOn = dayjs().subtract(1, 'minute').toDate();
+      const meetingStartsAt = dayjs().add(2, 'day').toDate();
+      const meetingTitle = (await MeetingTitle.create(faker.random.alphaNumeric(50))).unwrap();
+      const isOrganizer = true;
+
+      const attendeeRes = Attendee.create({
+        username,
+        fullName,
+        meetingID,
+        joinedMeetingOn,
+        meetingStartsAt,
+        meetingTitle,
+        isOrganizer,
+      });
+      expect(attendeeRes.isErr()).toBe(false);
+      expect(attendeeRes.unwrap()).toBeInstanceOf(Attendee);
+      expect(attendeeRes.unwrap().username).toBe(username);
+      expect(attendeeRes.unwrap().fullName).toBe(fullName);
+      expect(attendeeRes.unwrap().meetingID).toBe(meetingID);
+      expect(attendeeRes.unwrap().joinedMeetingOn).toBe(joinedMeetingOn);
+      expect(attendeeRes.unwrap().meetingStartsAt).toBe(meetingStartsAt);
+      expect(attendeeRes.unwrap().meetingTitle).toBe(meetingTitle);
+      expect(attendeeRes.unwrap().isOrganizer).toBe(isOrganizer);
+    });
+
+    it('return a new attendee given mandatory args only', async () => {
+      expect.hasAssertions();
+
+      const username = (await UserName.create(faker.random.alphaNumeric(6))).unwrap();
+      const fullName = (await UserFullName.create(faker.name.findName())).unwrap();
+      const meetingID = MeetingID.create();
+      const joinedMeetingOn = dayjs().subtract(1, 'minute').toDate();
+      const meetingStartsAt = dayjs().add(2, 'day').toDate();
+      const meetingTitle = (await MeetingTitle.create(faker.random.alphaNumeric(50))).unwrap();
+
+      const attendeeRes = Attendee.create({
+        username,
+        fullName,
+        meetingID,
+        joinedMeetingOn,
+        meetingStartsAt,
+        meetingTitle,
+      });
+      expect(attendeeRes.isErr()).toBe(false);
+      expect(attendeeRes.unwrap()).toBeInstanceOf(Attendee);
+      expect(attendeeRes.unwrap().username).toBe(username);
+      expect(attendeeRes.unwrap().fullName).toBe(fullName);
+      expect(attendeeRes.unwrap().meetingID).toBe(meetingID);
+      expect(attendeeRes.unwrap().joinedMeetingOn).toBe(joinedMeetingOn);
+      expect(attendeeRes.unwrap().meetingStartsAt).toBe(meetingStartsAt);
+      expect(attendeeRes.unwrap().meetingTitle).toBe(meetingTitle);
+      expect(attendeeRes.unwrap().isOrganizer).toBe(false);
+    });
+  });
+
+  describe('update meeting start time', () => {
+    it('successfully updates the start time', async () => {
+      expect.hasAssertions();
+
+      const username = (await UserName.create(faker.random.alphaNumeric(6))).unwrap();
+      const fullName = (await UserFullName.create(faker.name.findName())).unwrap();
+      const meetingID = MeetingID.create();
+      const joinedMeetingOn = dayjs().subtract(1, 'minute').toDate();
+      const oldMeetingStartsAt = dayjs().add(2, 'day').toDate();
+      const meetingTitle = (await MeetingTitle.create(faker.random.alphaNumeric(50))).unwrap();
+      const isOrganizer = true;
+
+      const attendee = Attendee.create({
+        username,
+        fullName,
+        meetingID,
+        joinedMeetingOn,
+        meetingStartsAt: oldMeetingStartsAt,
+        meetingTitle,
+        isOrganizer,
+      }).unwrap();
+
+      const newMeetingStartsAt = dayjs().add(4, 'day').toDate();
+      const updated = attendee.setMeetingStartsAt(newMeetingStartsAt);
+      expect(updated.isErr()).toBe(false);
+      expect(attendee.meetingStartsAt).toBe(newMeetingStartsAt);
+    });
+  });
+
+  describe('update meeting title', () => {
+    it('successfully updates the title', async () => {
+      expect.hasAssertions();
+
+      const username = (await UserName.create(faker.random.alphaNumeric(6))).unwrap();
+      const fullName = (await UserFullName.create(faker.name.findName())).unwrap();
+      const meetingID = MeetingID.create();
+      const joinedMeetingOn = dayjs().subtract(1, 'minute').toDate();
+      const meetingStartsAt = dayjs().add(2, 'day').toDate();
+      const oldMeetingTitle = (await MeetingTitle.create(faker.random.alphaNumeric(50))).unwrap();
+      const isOrganizer = true;
+
+      const attendee = Attendee.create({
+        username,
+        fullName,
+        meetingID,
+        joinedMeetingOn,
+        meetingStartsAt,
+        meetingTitle: oldMeetingTitle,
+        isOrganizer,
+      }).unwrap();
+
+      const newMeetingTitle = (await MeetingTitle.create(faker.random.alphaNumeric(30))).unwrap();
+      const updated = attendee.setMeetingTitle(newMeetingTitle);
+      expect(updated.isErr()).toBe(false);
+      expect(attendee.meetingTitle).toBe(newMeetingTitle);
+    });
+  });
+});
